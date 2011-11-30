1e17786d35e5d1fb4778d2ac4129ad756185a54e
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-11-30 17:11:53 +0900

    first commit (not prime number, is fib)

---
+++ PrimeNumbers/PrimeNumbers.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import qualified Prelude as P
+import Copilot.Language.Prelude
+import Copilot.Language
+
+fib :: Stream Word64
+fib = [0, 1] ++ fib + drop 1 fib
+
+fibSpec :: Spec
+fibSpec = do
+  trigger "fib_out" true [arg fib]
+
+main :: IO ()
+main = interpret 20 [] fibSpec
